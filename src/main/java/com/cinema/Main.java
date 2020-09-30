b322495b5a5313d9e05e184bff00b5845738a8ae
Kostian <pyteec.ua@i.ua>
2020-09-30 16:52:22 +0300

    added solution

---
+++ src/main/java/com/cinema/Main.java
@@ -0,0 +1,22 @@
+package com.cinema;
+
+import com.cinema.lib.Injector;
+import com.cinema.model.Movie;
+import com.cinema.service.MovieService;
+
+public class Main {
+    private static Injector injector = Injector.getInstance("com.cinema");
+
+    public static void main(String[] args) {
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+
+        Movie movie = new Movie();
+        movieService.getAll().forEach(System.out::println);
+
+        movie.setTitle("Fast and Furious");
+        movie.setDescription("Action movie about cars, races and fights");
+        movieService.add(movie);
+
+        movieService.getAll().forEach(System.out::println);
+    }
+}
---
+++ src/main/java/com/cinema/dao/impl/MovieDaoImpl.java
@@ -0,0 +1,45 @@
+package com.cinema.dao.impl;
+
+import com.cinema.dao.MovieDao;
+import com.cinema.lib.Dao;
+import com.cinema.model.Movie;
+import com.cinema.util.HibernateUtil;
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+@Dao
+public class MovieDaoImpl implements MovieDao {
+    @Override
+    public Movie add(Movie movie) {
+        Transaction transaction = null;
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.save(movie);
+            transaction.commit();
+            return movie;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't add movie " + movie.getTitle());
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public List<Movie> getAll() {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Query<Movie> getAllMoviesQuery = session.createQuery("from Movie", Movie.class);
+            return getAllMoviesQuery.getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't get all movies");
+        }
+    }
+}
