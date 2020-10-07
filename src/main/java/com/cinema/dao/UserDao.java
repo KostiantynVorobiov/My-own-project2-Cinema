47e33f12f2b809675f96721f8fe968983e0bd7bb
Kostian <pyteec.ua@i.ua>
2020-10-07 16:41:21 +0300

    save

---
+++ src/main/java/com/cinema/dao/UserDao.java
@@ -0,0 +1,11 @@
+package com.cinema.dao;
+
+import com.cinema.model.User;
+
+import java.util.Optional;
+
+public interface UserDao {
+    User add(User user);
+
+    Optional<User> findByEmail(String email);
+}
---
+++ src/main/java/com/cinema/security/AuthenticationServiceImpl.java
@@ -0,0 +1,37 @@
+package com.cinema.security;
+
+import com.cinema.exeption.AuthenticationException;
+import com.cinema.lib.Inject;
+import com.cinema.lib.Service;
+import com.cinema.model.User;
+import com.cinema.service.UserService;
+import com.cinema.util.HashUtil;
+
+import java.util.Optional;
+
+@Service
+public class AuthenticationServiceImpl implements AuthenticationService{
+
+    @Inject
+    private UserService userService;
+
+    @Override
+    public User login(String email, String password) throws AuthenticationException {
+        Optional<User> userFromDb = userService.findByEmail(email);
+        if(userFromDb.isPresent() && userFromDb.get().getPassword().equals(HashUtil.
+                hashPassword(password, userFromDb.get().getSalt()))) {
+            return userFromDb.get();
+        }
+        throw new AuthenticationException("Incorrect login or password");
+    }
+
+    @Override
+    public User register(String email, String password) {
+        User user = new User();
+        user.setEmail(email);
+        user.setSalt(HashUtil.getSalt());
+        user.setPassword(HashUtil.hashPassword(password, user.getSalt()));
+        userService.add(user);
+        return user;
+    }
+}
