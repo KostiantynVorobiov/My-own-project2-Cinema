3b21670f69629f7377c30aa67dbb35f84529b8a7
Kostian <pyteec.ua@i.ua>
2020-10-15 17:12:13 +0300

    save

--- src/main/java/com/cinema/Main.java
+++ src/main/java/com/cinema/Main.java
@@ -1,5 +1,6 @@
 package com.cinema;
 
+import com.cinema.dao.impl.CinemaHallDaoImpl;
 import com.cinema.exeption.AuthenticationException;
 import com.cinema.lib.Injector;
 import com.cinema.model.CinemaHall;
@@ -13,16 +14,19 @@ import com.cinema.service.MovieSessionService;
 import com.cinema.service.OrderService;
 import com.cinema.service.ShoppingCartService;
 import com.cinema.service.UserService;
+import org.apache.log4j.Logger;
+
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public class Main {
     private static Injector injector = Injector.getInstance("com.cinema");
+    private static final Logger logger = Logger.getLogger(Main.class);
 
-    public static void main(String[] args) throws AuthenticationException {
+    public static void main(String[] args) {
         MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
         Movie furious = new Movie();
-        movieService.getAll().forEach(System.out::println);
+        movieService.getAll().forEach(logger::info);
         furious.setTitle("Fast and Furious");
         furious.setDescription("Action movie about cars, races and fights");
         movieService.add(furious);
@@ -30,8 +34,8 @@ public class Main {
         cars.setTitle("Cars");
         cars.setDescription("Cartoon for children");
         movieService.add(cars);
-        System.out.println("All movies");
-        movieService.getAll().forEach(System.out::println);
+        logger.info("All movies");
+        movieService.getAll().forEach(logger::info);
 
         CinemaHallService cinemaHallService = (CinemaHallService)
                 injector.getInstance(CinemaHallService.class);
@@ -43,7 +47,7 @@ public class Main {
         cinemaHallFamily.setCapacity(100);
         cinemaHallFamily.setDescription("for family");
         cinemaHallService.add(cinemaHallFamily);
-        cinemaHallService.getAll().forEach(System.out::println);
+        cinemaHallService.getAll().forEach(logger::info);
 
         MovieSession movieSession1 = new MovieSession();
         movieSession1.setCinemaHall(cinemaHallHorror);
@@ -53,7 +57,7 @@ public class Main {
                 injector.getInstance(MovieSessionService.class);
         movieSessionService.add(movieSession1);
         movieSessionService.findAvailableSessions(furious.getId(),
-                LocalDate.now()).forEach(System.out::println);
+                LocalDate.now()).forEach(logger::info);
 
         MovieSession movieSession2 = new MovieSession();
         movieSession2.setCinemaHall(cinemaHallFamily);
@@ -61,7 +65,7 @@ public class Main {
         movieSession2.setShowTime(LocalDateTime.now().plusDays(1));
         movieSessionService.add(movieSession2);
         movieSessionService.findAvailableSessions(cars.getId(),
-                LocalDate.now().plusDays(1)).forEach(System.out::println);
+                LocalDate.now().plusDays(1)).forEach(logger::info);
 
         User panas = new User();
         panas.setEmail("panas@u.com");
@@ -74,34 +78,39 @@ public class Main {
         onic.setPassword("911");
         UserService userService = (UserService) injector.getInstance(UserService.class);
         authenticationService.register(onic.getEmail(), onic.getPassword());
-        authenticationService.login(onic.getEmail(), onic.getPassword());
-        System.out.println(userService.findByEmail(onic.getEmail()).get());
+ //       authenticationService.login(onic.getEmail(), onic.getPassword());
+        logger.info("User by email " + userService.findByEmail(onic.getEmail()).get());
         User foundUser = userService.findByEmail("onic@u.com").get();
-        System.out.println("My found user: " + foundUser);
+        logger.info("My found user: " + foundUser);
 
         ShoppingCartService shoppingCartService =
                 (ShoppingCartService) injector.getInstance(ShoppingCartService.class);
         authenticationService.register("emily@com", "12345");
-        User emily = authenticationService.login("emily@com", "12345");
+        User emily = null;
+        try {
+            emily = authenticationService.login("emily@com", "12345");
+        } catch (AuthenticationException e) {
+            logger.error(e);
+        }
         shoppingCartService.addSession(movieSession1, emily);
-        System.out.println("Get cart by Emily user: " + shoppingCartService.getByUser(emily));
+        logger.info("Get cart by Emily user: " + shoppingCartService.getByUser(emily));
         shoppingCartService.clear(shoppingCartService.getByUser(emily));
-        System.out.println("Get cart by Emily user after clear: "
+        logger.info("Get cart by Emily user after clear: "
                 + shoppingCartService.getByUser(emily));
         shoppingCartService.registerNewShoppingCart(panas);
         shoppingCartService.addSession(movieSession1, panas);
         shoppingCartService.addSession(movieSession2, foundUser);
-        System.out.println("Get cart by panas user: " + shoppingCartService.getByUser(panas));
+        logger.info("Get cart by Panas user: " + shoppingCartService.getByUser(panas));
         shoppingCartService.clear(shoppingCartService.getByUser(panas));
-        System.out.println("Get cart by panas user after clear: "
+        logger.info("Get cart by Panas user after clear: "
                 + shoppingCartService.getByUser(panas));
 
         OrderService orderService = (OrderService) injector.getInstance(OrderService.class);
         orderService.completeOrder(shoppingCartService.getByUser(foundUser).getTickets(),
                 foundUser);
 
-        System.out.println("All orders in found user he is Onic"
+        logger.info("All orders in found user he is Onic"
                 + orderService.getOrderHistory(foundUser));
-        orderService.getOrderHistory(foundUser).forEach(System.out::println);
+        orderService.getOrderHistory(foundUser).forEach(logger::info);
     }
 }
--- src/main/java/com/cinema/dao/impl/MovieDaoImpl.java
+++ src/main/java/com/cinema/dao/impl/MovieDaoImpl.java
@@ -6,14 +6,18 @@ import com.cinema.lib.Dao;
 import com.cinema.model.Movie;
 import com.cinema.util.HibernateUtil;
 import java.util.List;
+
+import org.apache.log4j.Logger;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 
 @Dao
 public class MovieDaoImpl implements MovieDao {
+    private static final Logger logger = Logger.getLogger(MovieDaoImpl.class);
     @Override
     public Movie add(Movie movie) {
+        logger.info("Trying to add movie");
         Transaction transaction = null;
         Session session = null;
         try {
@@ -36,6 +40,7 @@ public class MovieDaoImpl implements MovieDao {
 
     @Override
     public List<Movie> getAll() {
+        logger.info("Trying to get all movies");
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             Query<Movie> getAllMoviesQuery = session.createQuery("FROM Movie", Movie.class);
             return getAllMoviesQuery.getResultList();
