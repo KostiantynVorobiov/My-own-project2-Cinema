c2716a91f97475c114b19a55237adbfdffd84fc2
Kostian <pyteec.ua@i.ua>
2020-10-28 23:51:33 +0200

    added validator

--- src/main/java/com/cinema/model/dto/UserRequestDto.java
+++ src/main/java/com/cinema/model/dto/UserRequestDto.java
@@ -1,9 +1,14 @@
 package com.cinema.model.dto;
 
+import com.cinema.validator.ValidEmail;
+import com.cinema.validator.ValidPassword;
 import lombok.Data;
 
 @Data
+@ValidPassword
 public class UserRequestDto {
+    @ValidEmail
     private String email;
     private String password;
+    private String repeatPassword;
 }
---
+++ src/main/java/com/cinema/validator/PasswordValidator.java
@@ -0,0 +1,16 @@
+package com.cinema.validator;
+
+import com.cinema.model.dto.UserRequestDto;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class PasswordValidator implements ConstraintValidator<ValidPassword, UserRequestDto> {
+
+    @Override
+    public boolean isValid(UserRequestDto userRequestDto,
+                           ConstraintValidatorContext constraintValidatorContext) {
+        String password = userRequestDto.getPassword();
+        String repeatPassword = userRequestDto.getRepeatPassword();
+        return password != null && password.equals(repeatPassword);
+    }
+}
